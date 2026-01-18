25143b9e7d18a6fc167019d0223844f57a89f712
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2026-01-17 17:05:47 -0800

    Add files via upload

--- src/access.rs
+++ src/access.rs
@@ -1,22 +1,46 @@
-impl<T:GetInner<U>,U> GetInner<Option<U>> for Option<T>{
-	fn get_inner(&self)->Option<U>{self.as_ref().map(T::get_inner)}
-}
-impl<T:GetInner<U>,U> GetInner<U> for Arc<T>{
+direct_ref!(@inner Slice<T>);
+direct_ref!(@inner Vec<T>);
+direct_ref!(@inner bool);
+direct_ref!(@inner char);
+direct_ref!(@inner f32);
+direct_ref!(@inner f64);
+direct_ref!(@inner i128);
+direct_ref!(@inner i16);
+direct_ref!(@inner i32);
+direct_ref!(@inner i64);
+direct_ref!(@inner i8);
+direct_ref!(@inner isize);
+direct_ref!(@inner u128);
+direct_ref!(@inner u16);
+direct_ref!(@inner u32);
+direct_ref!(@inner u64);
+direct_ref!(@inner u8);
+direct_ref!(@inner usize);
+direct_ref!(@mut Slice<T>);
+direct_ref!(@mut Vec<T>);
+direct_ref!(@mut bool);
+direct_ref!(@mut char);
+direct_ref!(@mut f32);
+direct_ref!(@mut f64);
+direct_ref!(@mut i128);
+direct_ref!(@mut i16);
+direct_ref!(@mut i32);
+direct_ref!(@mut i64);
+direct_ref!(@mut i8);
+direct_ref!(@mut isize);
+direct_ref!(@mut u128);
+direct_ref!(@mut u16);
+direct_ref!(@mut u32);
+direct_ref!(@mut u64);
+direct_ref!(@mut u8);
+direct_ref!(@mut usize);
+impl<T:?Sized+GetInner<U>,U> GetInner<U> for Arc<T>{
 	fn get_inner(&self)->U{(**self).get_inner()}
 }
-impl<T:Inner<U>,U> Inner<U> for Arc<T>{
+impl<T:?Sized+Inner<U>,U:?Sized> Inner<U> for Arc<T>{
 	fn inner(&self)->&U{(**self).inner()}
 }
-impl<T:InnerRef<U>,U> InnerRef<Option<U>> for Option<T>{
-	fn inner_ref(&self)->Self::Ref<'_>{
-		match self{
-			None=>None,
-			Some(t)=>Some(t.inner_ref())
-		}
-	}
-	type Ref<'a>=Option<T::Ref<'a>> where T:'a,U:'a;
-}
-impl<T:InnerRefMut<U>,U> InnerRefMut<Option<U>> for Arc<T>{
+impl<T:?Sized+InnerRefMut<U>,U:?Sized> InnerRefMut<U> for Arc<T>{
 	fn inner_ref_mut(&mut self)->Self::RefMut<'_>{
 		match Arc::get_mut(self){
 			None=>None,
@@ -25,7 +49,19 @@ impl<T:InnerRefMut<U>,U> InnerRefMut<Option<U>> for Arc<T>{
 	}
 	type RefMut<'a>=Option<T::RefMut<'a>> where T:'a,U:'a;
 }
-impl<T:InnerRefMut<U>,U> InnerRefMut<Option<U>> for Option<T>{
+impl<T:GetInner<U>,U> GetInner<Option<U>> for Option<T>{
+	fn get_inner(&self)->Option<U>{self.as_ref().map(T::get_inner)}
+}
+impl<T:InnerRef<U>,U:?Sized> InnerRef<U> for Option<T>{
+	fn inner_ref(&self)->Self::Ref<'_>{
+		match self{
+			None=>None,
+			Some(t)=>Some(t.inner_ref())
+		}
+	}
+	type Ref<'a>=Option<T::Ref<'a>> where T:'a,U:'a;
+}
+impl<T:InnerRefMut<U>,U:?Sized> InnerRefMut<U> for Option<T>{
 	fn inner_ref_mut(&mut self)->Self::RefMut<'_>{
 		match self{
 			None=>None,
@@ -34,16 +70,54 @@ impl<T:InnerRefMut<U>,U> InnerRefMut<Option<U>> for Option<T>{
 	}
 	type RefMut<'a>=Option<T::RefMut<'a>> where T:'a,U:'a;
 }
+impl<T:UnwrapInner<U>,U> UnwrapInner<Option<U>> for Arc<T>{
+	fn unwrap_inner(self)->Option<U>{Arc::into_inner(self).map(T::unwrap_inner)}
+}
 impl<T:UnwrapInner<U>,U> UnwrapInner<Option<U>> for Option<T>{
 	fn unwrap_inner(self)->Option<U>{self.map(T::unwrap_inner)}
 }
-impl<T:?Sized+Inner<U>,U:?Sized> InnerRef<U> for T{
+/*impl<T:?Sized+Inner<U>,U:?Sized> InnerRef<U> for T{
 	fn inner_ref(&self)->Self::Ref<'_>{self.inner()}
 	type Ref<'a>=&'a U where Self:'a,U:'a;
 }
 impl<T:?Sized+InnerMut<U>,U:?Sized> InnerRefMut<U> for T{
 	fn inner_ref_mut(&mut self)->Self::RefMut<'_>{self.inner_mut()}
 	type RefMut<'a>=&'a mut U where Self:'a,U:'a;
+}*/
+#[macro_export]
+/// uses inner to implement inner ref
+macro_rules! direct_ref{
+	(@inner $name:ident$(<$($param:ident)*>)?$(where $($bound:tt)*)?)=>(
+		impl<T307EC305B4556985:?Sized,$($($param)*)?> InnerRef<T307EC305B4556985> for $name$(<$($param)*>)? where Self:Inner<T307EC305B4556985>,$($($bound)*)?{
+			fn inner_ref(&self)->Self::Ref<'_>{self.inner()}
+			type Ref<'a>=&'a T307EC305B4556985 where Self:'a,T307EC305B4556985:'a;
+		}
+	);
+	(@mut $name:ident$(<$($param:ident)*>)?$(where $($bound:tt)*)?)=>(
+		impl<T307EC305B4556985:?Sized,$($($param)*)?> InnerRefMut<T307EC305B4556985> for $name$(<$($param)*>)? where Self:InnerMut<T307EC305B4556985>,$($($bound)*)?{
+			fn inner_ref_mut(&mut self)->Self::RefMut<'_>{self.inner_mut()}
+			type RefMut<'a>=&'a mut T307EC305B4556985 where Self:'a,T307EC305B4556985:'a;
+		}
+	);
+}
+#[macro_export]
+/// implement get inner, inner, inner mut, unwrap inner for a type such that it's its own inner value
+macro_rules! self_inner{
+	(@get $name:ident$(<$($param:ident)*>)?$(where $($bound:tt)*)?)=>(
+		impl $(<$($param)*>)? GetInner<Self> for $name$(<$($param)*>)? where Self:Clone,$($($bound)*)?{
+			fn get_inner(&self)->Self{self.clone()}
+		}
+	);
+	(@inner $name:ident$(<$($param:ident)*>)?$(where $($bound:tt)*)?)=>(
+		impl $(<$($param)*>)? Inner<Self> for $name$(<$($param)*>)? $(where $($bound)*)?{
+			fn inner(&self)->&Self{self}
+		}
+	);
+	(@mut $name:ident$(<$($param:ident)*>)?$(where $($bound:tt)*)?)=>(
+		impl $(<$($param)*>)? InnerMut<Self> for $name$(<$($param)*>)? $(where $($bound)*)?{
+			fn inner_mut(&mut self)->&mut Self{self}
+		}
+	);
 }
 /// declares function for getting a presumably owned inner value
 pub trait GetInner<T>{
@@ -79,4 +153,59 @@ pub trait UnwrapInner<T>{
 	/// unwraps the inner value
 	fn unwrap_inner(self)->T;
 }
+self_inner!(@get Vec<T>);
+self_inner!(@get bool);
+self_inner!(@get char);
+self_inner!(@get f32);
+self_inner!(@get f64);
+self_inner!(@get i128);
+self_inner!(@get i16);
+self_inner!(@get i32);
+self_inner!(@get i64);
+self_inner!(@get i8);
+self_inner!(@get isize);
+self_inner!(@get u128);
+self_inner!(@get u16);
+self_inner!(@get u32);
+self_inner!(@get u64);
+self_inner!(@get u8);
+self_inner!(@get usize);
+self_inner!(@inner Slice<T>);
+self_inner!(@inner Vec<T>);
+self_inner!(@inner bool);
+self_inner!(@inner char);
+self_inner!(@inner f32);
+self_inner!(@inner f64);
+self_inner!(@inner i128);
+self_inner!(@inner i16);
+self_inner!(@inner i32);
+self_inner!(@inner i64);
+self_inner!(@inner i8);
+self_inner!(@inner isize);
+self_inner!(@inner u128);
+self_inner!(@inner u16);
+self_inner!(@inner u32);
+self_inner!(@inner u64);
+self_inner!(@inner u8);
+self_inner!(@inner usize);
+self_inner!(@mut Slice<T>);
+self_inner!(@mut Vec<T>);
+self_inner!(@mut bool);
+self_inner!(@mut char);
+self_inner!(@mut f32);
+self_inner!(@mut f64);
+self_inner!(@mut i128);
+self_inner!(@mut i16);
+self_inner!(@mut i32);
+self_inner!(@mut i64);
+self_inner!(@mut i8);
+self_inner!(@mut isize);
+self_inner!(@mut u128);
+self_inner!(@mut u16);
+self_inner!(@mut u32);
+self_inner!(@mut u64);
+self_inner!(@mut u8);
+self_inner!(@mut usize);
+type Slice<T>=[T];
+pub use {direct_ref,self_inner};
 use std::sync::Arc;
--- src/wrap.rs
+++ src/wrap.rs
@@ -1,25 +1,21 @@
-impl<T:?Sized> Inner<T> for WrappedInner<T>{
-	fn inner(&self)->&T{
-		let Self(t)=self;
-		t
-	}
-}
-impl<T:?Sized> InnerMut<T> for WrappedInner<T>{
-	fn inner_mut(&mut self)->&mut T{
-		let Self(t)=self;
-		t
-	}
-}
-impl<T> UnwrapInner<T> for WrappedInner<T>{
-	fn unwrap_inner(self)->T{
-		let Self(t)=self;
-		t
-	}
-}
+direct_ref!(@inner WrappedInner<T> where T:?Sized);
+direct_ref!(@mut WrappedInner<T> where T:?Sized);
+
 #[macro_export]
-/// creates a transparent wrapper and implements some important traits for it// TODO allow multiple types and where bounds, make the multi call thing work
+/// creates a transparent wrapper and implements some important traits for it TODO allow multiple types and where bounds, make the multi call thing work
 /// generic_transparent_wrapper!(@declare "comment" pub(somewhere) Wrapper<T>) declares the struct and derives builtin rust traits #[derive(Clone,Copy,Debug,Default,Eq,Hash,Ord,PartialEq,PartialOrd)], applies #[repr(transparent)], and documents with the comment
 /// generic_transparent_wrapper!(unsafe @from-ref Wrapper<T>) implements from &T for &Wrapper<T> and from &mut T for &mut Wrapper<T>. for safety, Wrapper<T> must be a transparent repr of T
+/// TODO document other branches
+/// example usage: creating WrappedInner
+
+/// generic_transparent_wrapper!(@declare "wrapper to make unwrap_inner and other access functions that might just call versions of themselves on the inner value stop here" pub WrappedInner<T> where T:?Sized);
+/// generic_transparent_wrapper!(@from WrappedInner<T>);
+/// generic_transparent_wrapper!(@get WrappedInner<T>.0);
+/// generic_transparent_wrapper!(@inner WrappedInner<T>.0 where T:?Sized);
+/// generic_transparent_wrapper!(@mut WrappedInner<T>.0 where T:?Sized);
+/// generic_transparent_wrapper!(@unwrap WrappedInner<T>.0 where T:?Sized);
+/// generic_transparent_wrapper!(unsafe @from-ref WrappedInner<T> where T:?Sized);
+
 macro_rules! generic_transparent_wrapper{
 	(@declare $($comment:literal)? $v:vis $name:ident<$param:ident>$(where $($bound:tt)*)?)=>(
 		#[derive(Clone,Copy,Debug,Default,Eq,Hash,Ord,PartialEq,PartialOrd)]
@@ -43,9 +39,19 @@ macro_rules! generic_transparent_wrapper{
 			fn from(value:$param)->Self{Self(value)}
 		}
 	);
-	(@get $($comment:literal)? $v:vis $name:ident<$param:ident>$(where $($bound:tt)*)?)=>(
+	(@get $($comment:literal)? $v:vis $name:ident<$param:ident>.$field:tt$(where $($bound:tt)*)?)=>(
 		impl<$param:Clone> GetInner<$param> for $name<$param>$(where $($bound)*)?{
-			fn get_inner(&self)->$param{self.0.clone()}
+			fn get_inner(&self)->$param{self.$field.clone()}
+		}
+	);
+	(@inner $($comment:literal)? $v:vis $name:ident<$param:ident>.$field:tt$(where $($bound:tt)*)?)=>(
+		impl<$param:Clone> Inner<$param> for $name<$param>$(where $($bound)*)?{
+			fn inner(&self)->&$param{&self.$field}
+		}
+	);
+	(@mut $($comment:literal)? $v:vis $name:ident<$param:ident>.$field:tt$(where $($bound:tt)*)?)=>(
+		impl<$param:Clone> InnerMut<$param> for $name<$param>$(where $($bound)*)?{
+			fn inner_mut(&mut self)->&mut $param{&mut self.$field}
 		}
 	);
 	(unsafe @from-ref $name:ident<$param:ident>$(where $($bound:tt)*)?)=>(
@@ -60,13 +66,22 @@ macro_rules! generic_transparent_wrapper{
 			}
 		}
 	);
-	($(@$variant:ident)* $name:ident<$param:ident>)=>($(
-		generic_transparent_wrapper!(@$variant $name<$param>);
-	)*);
+	(@unwrap $($comment:literal)? $v:vis $name:ident<$param:ident>.$field:tt$(where $($bound:tt)*)?)=>(
+		impl<$param:Sized> UnwrapInner<$param> for $name<$param>$(where $($bound)*)?{
+			fn unwrap_inner(self)->$param{self.$field}
+		}
+	);
 }
 generic_transparent_wrapper!(@declare "wrapper to make unwrap_inner and other access functions that might just call versions of themselves on the inner value stop here" pub WrappedInner<T> where T:?Sized);
-generic_transparent_wrapper!(@from @get WrappedInner<T>);
+generic_transparent_wrapper!(@from WrappedInner<T> where T:?Sized);
+generic_transparent_wrapper!(@get WrappedInner<T>.0);
+generic_transparent_wrapper!(@inner WrappedInner<T>.0 where T:?Sized);
+generic_transparent_wrapper!(@mut WrappedInner<T>.0 where T:?Sized);
+generic_transparent_wrapper!(@unwrap WrappedInner<T>.0);
 generic_transparent_wrapper!(unsafe @from-ref WrappedInner<T> where T:?Sized);
 pub use generic_transparent_wrapper;
+self_inner!(@get WrappedInner<T>);
+self_inner!(@inner WrappedInner<T> where T:?Sized);
+self_inner!(@mut WrappedInner<T> where T:?Sized);
 use core::mem;
 use crate::access::*;
